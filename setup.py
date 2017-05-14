c26ca6ab3dedcdc3598bd2ffb348b583bf75aaff
CraigLangford <craigllangford@gmail.com>
2017-05-14 17:11:17 +0100

    Updated status to automatically zip contents for Lambda function deployment

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from subprocess import call
+import shutil
+
+
+if __name__ == '__main__':
+    """
+    Running `python3 setup.py` will generate a cryptoprice.zip file which can
+    be uploaded to the AWS Lambda website and be used as a fully functioning
+    lambda function.
+    """
+    # 1. Include requests folder for distribution
+    call(['pip', 'install', 'requests', '-t', '.'])
+
+    # 2. Zip cryptoprice.py, requests/ and data/ into cryptoprice.zip
+    print("Adding contents to zip folder")
+    os.mkdir('zip_folder')
+    call(['cp', 'cryptoprice.py', 'zip_folder/'])
+    call(['cp', '-rf', 'data/', 'zip_folder/data/'])
+    call(['cp', '-rf', 'requests/', 'zip_folder/requests/'])
+    print("Zipping contents")
+    shutil.make_archive('cryptoprice', 'zip', 'zip_folder')
+
+    # 3. Remove requests folders.
+    print("Removing unneeded files")
+    call(['rm', '-rf', 'zip_folder'])
+    for node in os.listdir():
+        if node.startswith('requests'):
+            shutil.rmtree(node)
