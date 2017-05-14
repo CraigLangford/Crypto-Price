f3962b67f7e5ae72efddeaae8b9f11363165330a
CraigLangford <craigllangford@gmail.com>
2017-05-14 23:27:30 +0100

    Updated setup.py comments

--- setup.py
+++ setup.py
@@ -7,27 +7,29 @@ BOLD = '\033[1m'
 
 if __name__ == '__main__':
     """
-    Running `python3 setup.py` will generate a cryptoprice.zip file which can
+    Running `python setup.py` will generate a cryptoprice.zip file which can
     be uploaded to the AWS Lambda website and be used as a fully functioning
     lambda function.
     """
-    # 1. Include requests folder for distribution
+    # Include requests folder for distribution
     call(['pip', 'install', 'requests', '-t', '.'])
 
-    # 2. Zip cryptoprice.py, requests/ and data/ into cryptoprice.zip
+    # Place cryptoprice.py, data and requests into zip_folder
     print("Adding contents to zip folder")
     os.mkdir('zip_folder')
     call(['cp', 'cryptoprice.py', 'zip_folder/'])
     call(['cp', '-rf', 'data/', 'zip_folder/data/'])
     call(['cp', '-rf', 'requests/', 'zip_folder/requests/'])
+
+    # Zip contents of zip_folder
     print("Zipping contents")
     shutil.make_archive('cryptoprice', 'zip', 'zip_folder')
 
-    # 3. Remove requests folders.
+    # Remove folders used to create zip
     print("Removing unneeded files")
     call(['rm', '-rf', 'zip_folder'])
     for node in os.listdir():
-        if node.startswith('requests'):
+        if node.startswith('requests') or node == 'zip_folder':
             shutil.rmtree(node)
 
     print(BOLD + "cryptoprice.zip is now created - upload this to AWS Lambda"
